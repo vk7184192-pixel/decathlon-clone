0fe0ab8001104be137ba3f5684dcb24af85d2232
rahulch82667-maker <rahulch82667@gmail.com>
2026-09-01 09:55:19 +0530

    Update API configuration

--- admin-frontend/src/api/axios.js
+++ admin-frontend/src/api/axios.js
@@ -1,22 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
-});
-
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("adminToken");
-
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-
-    return config;
+  baseURL: "https://decathlon-clone-pi.vercel.app/api",
+  headers: {
+    "Content-Type": "application/json",
   },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
+});
 
 export default api;
--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,10 +1,26 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://decathlon-clone-qqzk8et2s-vk7184192-pixel.vercel.app/api",
   headers: {
     "Content-Type": "application/json",
   },
 });
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("adminToken");
+
+    // Don't send admin token while logging in
+    if (token && !config.url?.includes("/auth/login")) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
 
 export default api;
